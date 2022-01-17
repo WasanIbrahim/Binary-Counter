76f342f46486b09b6a1d51d15b1dff51bf55d075
wasan <wasan.ibrahiim@gmail.com>
2022-01-17 15:07:35 +0300

    first commit

---
+++ customCell/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  customCell
+//
+//  Created by Wa ibra. on 01/03/1443 AH.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
